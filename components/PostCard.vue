ca8726205ae0315f81d8b7b4dc6286c491137f57
Michael Wallner <m.wallner@neonblack.at>
2024-02-22 21:24:27 +0100

    ✨ add card to render blog posts

---
+++ components/PostCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import dayjs from 'dayjs'
+import type { PostStoryblok } from '~/storyblok/types'
+
+const localePath = useLocalePath()
+
+defineProps<{ post: PostStoryblok, slug: string }>()
+
+</script>
+
+<template>
+  <NuxtLink
+    :to="localePath(`/${slug}`)"
+    class="group space-y-4 hover:text-red-600"
+  >
+    <div class="overflow-clip">
+      <img
+        :src="`${post.content.teaser.filename}/m/743x417`"
+        :alt="post.content.teaser.alt"
+        class="w-full transition-transform duration-500 ease-quart-out group-hover:ease-circ-out group-hover:rotate-2 group-hover:scale-110"
+      >
+    </div>
+    <div>
+      <h3 class="text-xl font-bold mb-1">
+        {{ post.name }}
+      </h3>
+      <div class="text-gray-500">
+        {{ dayjs(post.created_at).format('MMMM DD, YYYY') }}
+      </div>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped>
+
+</style>
