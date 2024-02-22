daf9618139e83d533ea3d83a16028ab10b083538
Michael Wallner <m.wallner@neonblack.at>
2024-02-22 15:35:38 +0100

    ✨ add storyblok config to manage menus

---
+++ storyblok/Config.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <AppHeader/>
+    <div class="grow"></div>
+    <AppFooter/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
