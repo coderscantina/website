6363722c23abeed320822113b8ffad46f2a73895
Michael Wallner <m.wallner@neonblack.at>
2024-02-22 21:20:37 +0100

    👔 prepare AppHeader and AppFooter for bigger website

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,15 +1,45 @@
 <script setup lang="ts">
 
+const { config } = useStoryblokHelpers()
 const year = computed(() => new Date().getFullYear())
 
 </script>
 
 <template>
-  <div class="bg-black py-10">
-    <div class="content-grid">
+  <div class="bg-black py-12">
+    <div
+      v-if="config"
+      class="content-grid space-y-12"
+    >
+      <div class="md:flex">
+        <div class="space-y-6">
+          <img
+            src="@/assets/logo.svg?url"
+            alt="The Coder's Cantina"
+            class="h-12 w-12"
+          />
+          <p
+            v-editable="config.content"
+            class="max-w-prose mr-12"
+          >
+            {{ config.content.footer_about }}
+          </p>
+        </div>
+        <div class="flex flex-col md:flex-row flex-wrap gap-x-12 gap-y-12 mt-12 md:mt-0 ml-auto">
+          <StoryblokComponent
+            v-for="menu in config.content.footerMenus"
+            :key="menu._uid"
+            v-editable="menu"
+            :blok="menu"
+          />
+        </div>
+      </div>
       <div class="space-x-4">
-        <div class="text-center">
-          {{ $t('footer.copyright', { year }) }}
+        <div
+          v-editable="config.content"
+          class="text-center text-sm"
+        >
+          {{ config?.content?.copyright.replace(/{year}/g, year) }}
         </div>
       </div>
     </div>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,20 +1,54 @@
 <script setup lang="ts">
+
 const localePath = useLocalePath()
+const { config } = useStoryblokHelpers()
+
+const menu = computed(() => {
+  return config.value?.content.menu
+})
+
+const buttons = computed(() => {
+  return config.value?.content.buttons
+})
 
 </script>
 
 <template>
   <div class="absolute w-full z-10">
-    <div class="content-grid">
-      <div class="flex justify-between">
-        <NuxtLink :to="localePath('/')">
+    <div class="content-grid pt-6">
+      <div class="flex space-x-6 md:space-x-10 items-center">
+        <NuxtLink
+          :to="localePath('/')"
+          class="shrink-0"
+        >
           <img
             src="@/assets/logo.svg?url"
             alt="The Coder's Cantina"
-            class="h-20 w-20 mt-4"
+            class="h-12 w-12"
           />
         </NuxtLink>
-        <div>
+        <div class="flex space-x-6 md:space-x-10 grow">
+          <NuxtLink
+            v-for="item in menu"
+            :key="item._uid"
+            v-editable="item"
+            :to="localePath(`/${item.link.cached_url}`)"
+            class="font-semibold text-gray-500 hover:text-red-600"
+            active-class="text-red-600"
+          >
+            {{ item.label }}
+          </NuxtLink>
+        </div>
+        <div
+          v-if="buttons?.length > 0"
+          class="flex ml-auto space-y-6 items-stretch"
+        >
+          <StoryblokComponent
+            v-for="item in buttons"
+            :key="item._uid"
+            v-editable="item"
+            :blok="item"
+          />
         </div>
       </div>
     </div>
