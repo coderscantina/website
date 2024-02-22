185515548511dc5fca6baab87d9d3b0a080af47f
Michael Wallner <m.wallner@neonblack.at>
2024-02-22 21:20:03 +0100

    ✨ add FooterMenu, Picture, Snippet bloks

---
+++ storyblok/FooterMenu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import type { FooterMenuStoryblok } from '~/storyblok/types'
+
+defineProps<{ blok: FooterMenuStoryblok }>()
+
+</script>
+
+<template>
+  <div class="grow shrink-0 basis-full sm:basis-0">
+    <h4 class="uppercase text-xs font-bold mb-6 tracking-wider">
+      {{ blok.header }}
+    </h4>
+    <div class="space-y-3 flex flex-col">
+      <NuxtLink
+        v-for="item in blok.menu"
+        :key="item._uid"
+        v-editable="item"
+        :href="item.link?.url"
+        :target="item.link?.target"
+        class="font-semibold text-gray-500 hover:text-red-600 focus-visible:text-red-600 transition-colors"
+        active-class="text-red-600"
+      >
+        {{ item.label }}
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ storyblok/Picture.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { PictureStoryblok } from '~/storyblok/types'
+
+defineProps<{ blok: PictureStoryblok }>()
+
+</script>
+
+<template>
+  <ContentWrapper
+    v-editable="blok"
+    :class-name="blok.class"
+  >
+    <img
+      :src="blok.image.filename"
+      :alt="blok.image.alt"
+      :width="blok.image.width"
+      :height="blok.image.height"
+    />
+    <p
+      v-if="blok.note"
+      class="mt-4 text-center text-gray-500 text-sm"
+    >{{ blok.note }}</p>
+  </ContentWrapper>
+</template>
+
+<style scoped>
+
+</style>
