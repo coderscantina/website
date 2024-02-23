f02ec91db40170e406eb1250842bdf9ea32cf751
Michael Wallner <m.wallner@neonblack.at>
2024-02-23 13:34:04 +0100

    📱 adapt HomeHero on mobile

--- storyblok/HomeHero.vue
+++ storyblok/HomeHero.vue
@@ -8,24 +8,26 @@ defineProps<{ blok: HomeHeroStoryblok }>()
 <template>
   <div
     v-editable="blok"
-    class="py-20 mx-auto lg:max-w-full lg:py-32 lg:relative lg:min-h-screen "
+    class="py-20 mx-auto max-w-full lg:py-32 relative lg:min-h-screen"
   >
     <div class="content-grid">
-      <article class="lg:w-6/12">
-        <div class="mt-8 lg:mt-0 lg:absolute lg:inset-y-0 lg:right-0 lg:w-6/12">
+      <article class="lg:w-1/2">
+        <div
+          class="mt-0 absolute top-0 left-0 lg:left-auto lg:inset-y-0 right-0 lg:w-6/12 z-[-1]"
+        >
           <picture v-if="blok.image">
             <source
               media="(min-width: 1024px)"
               :srcset="`${blok.image.filename}/m/1080x1080`"
             />
             <img
-              :src="`${blok.image.filename}/m/544x366`"
+              :src="`${blok.image.filename}/m/768x432`"
               :alt="blok.image.alt"
-              class="h-56 w-full object-cover object-center sm:h-56 shadow-xl lg:inset-0 lg:h-full lg:shadow-none lg:rounded-none"
+              class="w-full object-cover object-center inset-0 lg:h-full"
             />
           </picture>
           <svg
-            class="hidden lg:block absolute inset-y-0 left-0 w-24 h-full"
+            class="absolute h-16 w-full lg:w-24 lg:h-full -rotate-180 bottom-0 lg:rotate-0"
             viewBox="0 0 100 100"
             preserveAspectRatio="none"
             fill="none"
@@ -38,7 +40,8 @@ defineProps<{ blok: HomeHeroStoryblok }>()
             />
           </svg>
         </div>
-        <div class="mt-8 lg:mt-16 space-y-10 max-w-prose">
+        <div class="aspect-video w-full -mt-16 md:mt-0 lg:hidden"></div>
+        <div class="-mt-8 lg:mt-16 space-y-10 max-w-prose">
           <Headline
             class="text-3xl sm:text-4xl font-bold text-gray-100 leading-tight"
             :headline="blok.header"
