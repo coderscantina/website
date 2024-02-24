10dfd696f39f0260801ab370e6df2a92997b75f8
Michael Wallner <m.wallner@neonblack.at>
2024-02-24 20:53:48 +0100

    🩹 fix footer z-index on home

--- storyblok/Home.vue
+++ storyblok/Home.vue
@@ -19,7 +19,7 @@ setMetaFromPage(props.blok)
       :key="bodyBlok._uid"
       :blok="bodyBlok"
     />
-    <div class="-mt-16">
+    <div class="-mt-16 relative z-10">
       <div class="content-grid">
         <div class="text-gray-400 text-xs uppercase tracking-widest">
           <NuxtLink
